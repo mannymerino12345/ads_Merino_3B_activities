751cd46199f6b33d9e038284a68c1e17562320ec
manny merino <mannymerino00@gmail.com>
2024-09-05 15:17:26 +0800

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,35 @@
 <?php
-
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/students/create', function () {
+    $student = new Student();
+    $student->firstname = 'Manny';
+    $student->lastname = 'Merino';
+    $student->email = 'mannymerino123@gmail.com';
+    $student->age = 12;
+    $student->save();
+    return 'Student Created!';
 });
+
+Route::get('/students', function () {
+    $students = Student::all();
+    return $students;
+});
+
+Route::get('/students/update', function () {
+    $student = Student::where('email','mannymerino123@gmail.com')->first();
+    $student->email = 'manny.merino@newemail.com';
+    $student->save();
+    return 'Student Update!';
+});
+
+Route::get('/students', function () {
+    $student = Student::where('email','manny.merino@newemail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
+
+?>
